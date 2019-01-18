7ed31372455b6c88d9939b49b4a02856b3aada1a
NickPurcell <nick.purcell117@gmail.com>
2019-01-18 01:25:14 -0800

    Update simplecam.py

--- simplecam.py
+++ simplecam.py
@@ -1,47 +1,52 @@
-
 import cv2
 import numpy
 
 def showcam():
+    # Capture the camera feed
     cam = cv2.VideoCapture(0)
-    #Initially let all intensities through the filter
-    maxInt = 255
+    # Loop pupil detection and print webcam
     while True:
-        if cv2.waitKey(1) == 43:
-            print("UP")
-            maxInt += 10
-        if cv2.waitKey(1) == 45:
-            maxInt -= 10
-            print("DOWN")
+        # Take each image from the webcam
         ret_val, img, = cam.read()
-        thresh1 = pupildetect(img, maxInt)
-        cv2.imshow('my webcam', thresh1)
+        # run pupil detection algorithm
+        hough_img = pupildetect(img)
+        # print the webcam
+        cv2.imshow('my webcam', hough_img)
+        # quit the webcam at esc
         if cv2.waitKey(1) == 27:
             break  # esc to quit
+    # close the window
     cv2.destroyAllWindows()
 
-def pupildetect(img, maxInt):
-    #Blurs the image to make it easier to detect
-    bimg = cv2.medianBlur(img, 5)
-    #Turn the image greyscale
-    gimg = cv2.cvtColor(bimg, cv2.COLOR_BGR2GRAY)
-    #Turn every pixel above maxInt white
-    ret, cimg = cv2.threshold(gimg, maxInt, 0, cv2.THRESH_TOZERO_INV)
-    ##thresh1 = cv2.adaptiveThreshold(thresh2,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,.5)
+# Pupil detection algorithm
+# Img, the source of the image
+# returns the webcam image with
+# hough circles printed on it
+def pupildetect(img):
+    # Blurs the image to make it easier to detect
+    blur_img = cv2.medianBlur(img, 5)
+    # Turn the image greyscale
+    grey_img = cv2.cvtColor(blur_img, cv2.COLOR_BGR2GRAY)
+    # Binary threshold (source, threshold, white color, type)
+    # Currently using trunc that turns values over threshold pure white
+    ret, thresh_img = cv2.threshold(grey_img, 75, 255, cv2.THRESH_TRUNC)
     try:
-        circles = cv2.HoughCircles(cimg, cv2.HOUGH_GRADIENT, 1, 20,
+        # Create hough cricles
+        circles = cv2.HoughCircles(thresh_img, cv2.HOUGH_GRADIENT, 1, 20,
                                    param1=10, param2=30, minRadius=0, maxRadius=30)
         circles = numpy.uint16(numpy.around(circles))
+        # Draw circles
         for i in circles[0, :]:
             # draw the outer circle
-            cv2.circle(cimg, (i[0], i[1]), i[2], (0, 255, 0), 2)
+            cv2.circle(img, (i[0], i[1]), i[2], (0, 255, 0), 2)
             # draw the center of the circle
-            cv2.circle(cimg, (i[0], i[1]), 2, (0, 0, 255), 3)
+            cv2.circle(img, (i[0], i[1]), 2, (0, 0, 255), 3)
+    # sometimes there are no circles detected and the program poops
+    # out not quite sure how to stop it, there's got to be a way to
+    # skip something if no circles are detected, fix later
     except:
         print("circle drawing failed")
-    return cimg
-
-
+    return img
 
 def main():
     showcam()
