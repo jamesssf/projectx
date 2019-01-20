d5c2ae60713495443903ee8fb0058db064ad3243
jamesssf <30578100+jamesssf@users.noreply.github.com>
2019-01-20 11:14:06 -0800

    gstreamer

--- start_stream.sh
+++ start_stream.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 clear
-raspivid -n -t 0 -rot 270 -w 960 -h 720 -fps 30 -b 6000000 -o - | gst-launch-1.0 -e -vvvv fdsrc ! h264parse ! rtph264pay pt=96 config-interval=5 ! udpsink host=192.168.1.211 port=5000
+raspivid -n -t 0 -rot 270 -w 960 -h 720 -fps 30 -b 6000000 -o - | gst-launch-1.0 -e -vvvv fdsrc ! h264parse ! rtph264pay pt=96 config-interval=5 ! udpsink host=192.168.1.175 port=5000
