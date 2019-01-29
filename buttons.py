356db305743ad7d611996fbf4de560b59c0bcbbd
jamesssf <30578100+jamesssf@users.noreply.github.com>
2019-01-28 23:11:37 -0800

    Update buttons.py

--- buttons.py
+++ buttons.py
@@ -1,12 +1,7 @@
 from gpiozero import LED, Button
 from time import sleep
 
-led = LED(27)
-button = Button(23)
+led = LED(37)
 led.on()
 sleep(5)
-led.off()
-button.wait_for_press()
-led.on()
-sleep(3)
 led.off()
