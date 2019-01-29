b3e2b292b1db8904566614b0c2c2566da9debfd1
jamesssf <30578100+jamesssf@users.noreply.github.com>
2019-01-29 00:32:30 -0800

    Update buttons.py

--- buttons.py
+++ buttons.py
@@ -1,12 +1,10 @@
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 
 while True:
-    input_state_7 = GPIO.input(7)  # Bottom Button
-    if input_state_7:
+    input_state_23 = GPIO.input(23)  # Bottom Button
+    if input_state_23:
         print("Exit")
-    else:
-        print("not")
