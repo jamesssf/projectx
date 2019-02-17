3cded6fba0f715594c8a77a556081ced70f174c8
Nick Purcell <nick.purcell117@gmail.com>
2019-02-16 18:56:27 -0800

    Included instructions for use with different ips

--- start_stream.sh
+++ start_stream.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 clear
 raspivid -n -t 0 -rot 270 -w 960 -h 720 -fps 30 -b 6000000 -o - | gst-launch-1.0 -e -vvvv fdsrc ! h264parse ! rtph264pay pt=96 config-interval=5 ! udpsink host=192.168.1.211 port=5000
+#Change the host to the RECEIVING ip
