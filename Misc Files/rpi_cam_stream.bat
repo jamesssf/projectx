bf0b6639e0dbe1d874324bc4879d599dca7cbb73
Nick Purcell <nick.purcell117@gmail.com>
2019-02-16 18:53:17 -0800

    Included instructions for running on Linux

--- Misc Files/rpi_cam_stream.bat
+++ Misc Files/rpi_cam_stream.bat
@@ -1,3 +1,4 @@
 C:
 cd C:\gstreamer\1.0\x86_64\bin
 gst-launch-1.0 -e -v udpsrc port=5000 ! application/x-rtp, payload=96 ! rtpjitterbuffer ! rtph264depay ! avdec_h264 ! fpsdisplaysink sync=false text-overlay=false
+REM Just copy and paste the 2nd and 3rd lines in terminal
